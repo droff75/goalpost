ff1c909f2175495e8551bec5413c622a87e85cd3
David Roff <David.Roff@asynchrony.com>
2018-03-08 07:38:40 -0600

    added bind to keyboard for next button

--- goalpost-app/Controller/CreateGoalVC.swift
+++ goalpost-app/Controller/CreateGoalVC.swift
@@ -15,18 +15,28 @@ class CreateGoalVC: UIViewController {
     @IBOutlet weak var LongTermBtn: UIButton!
     @IBOutlet weak var nextBtn: UIButton!
     
+    var goalType: GoalType = .shortTerm
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        nextBtn.bindToKeyboard()
+        shortTermBtn.setSelectedColor()
+        LongTermBtn.setDeselectedColor()
     }
 
     @IBAction func nextBtnWasPressed(_ sender: Any) {
     }
 
     @IBAction func shortTermBtnWasPressed(_ sender: Any) {
+        goalType = .shortTerm
+        shortTermBtn.setSelectedColor()
+        LongTermBtn.setDeselectedColor()
     }
     
     @IBAction func longTermBtnWasPressed(_ sender: Any) {
+        goalType = .longTerm
+        LongTermBtn.setSelectedColor()
+        shortTermBtn.setDeselectedColor()
     }
     
     @IBAction func backBtnWasPressed(_ sender: Any) {
