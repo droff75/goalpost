e6409edd378c45df0145be541e4797c39e822b58
David Roff <David.Roff@asynchrony.com>
2018-03-12 09:12:55 -0500

    Added undo feature

--- goalpost-app/Controller/GoalsVC.swift
+++ goalpost-app/Controller/GoalsVC.swift
@@ -14,14 +14,17 @@ let appDelegate = UIApplication.shared.delegate as? AppDelegate
 class GoalsVC: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var goalRemovedView: UIView!
     
     var goals: [Goal] = []
+    var lastDeletedGoal: Goal!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
         tableView.isHidden = false
+        goalRemovedView.isHidden = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -46,15 +49,29 @@ class GoalsVC: UIViewController {
         guard let createGoalVC = storyboard?.instantiateViewController(withIdentifier: "CreateGoalVC") else { return }
         presentDetail(createGoalVC)
     }
+    
+    @IBAction func onUndoBtnPressed(_ sender: Any) {
+        restoreGoal()
+        setView(view: goalRemovedView, hidden: true)
+        tableView.reloadData()
+    }
+    
+    func setView(view: UIView, hidden: Bool) {
+        UIView.transition(with: view, duration: 0.5, options: .transitionCrossDissolve, animations: {
+            view.isHidden = hidden
+        })
+    }
 }
 
 extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return goals.count
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell") as? GoalCell else { return UITableViewCell() }
         let goal = goals[indexPath.row]
@@ -75,6 +92,14 @@ extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
             self.removeGoal(atIndexPath: indexPath)
             self.fetchCoreDataObjects()
             tableView.deleteRows(at: [indexPath], with: .automatic)
+            
+            self.setView(view: self.goalRemovedView, hidden: false)
+            
+            let dismissTime = DispatchTime.now() + 5
+            DispatchQueue.main.asyncAfter(deadline: dismissTime) {
+                self.setView(view: self.goalRemovedView, hidden: true)
+            }
+            
         }
         
         let addAction = UITableViewRowAction(style: .normal, title: "ADD 1") { (rowAction, indexPath) in
@@ -112,6 +137,7 @@ extension GoalsVC {
     func removeGoal(atIndexPath indexPath: IndexPath) {
         guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
         
+        managedContext.undoManager = UndoManager()
         managedContext.delete(goals[indexPath.row])
         
         do {
@@ -122,6 +148,14 @@ extension GoalsVC {
         }
     }
     
+    func restoreGoal() {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        
+        managedContext.undoManager?.undo()
+        setView(view: goalRemovedView, hidden: true)
+        self.fetchCoreDataObjects()
+        tableView.reloadData()
+    }
     
     func fetch(completion: (_ complete: Bool) -> ()) {
         guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
